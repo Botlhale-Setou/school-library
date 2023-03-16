88495c102dd0fb01bd4fdf6ca15dc3737b876a41
Botlhale Setou <botlhale.setou@gmail.com>
2023-03-16 21:06:21 +0200

    fix linter errors

--- app.rb
+++ app.rb
@@ -26,20 +26,25 @@ class App
     end
   end
 
+  def create_student
+    print 'Enter the student name: '
+    name = gets.chomp
+    print 'Enter the student\'s age: '
+    age = gets.chomp
+    print 'Does the student have parental permission? (true/false): '
+    permitted = gets.chomp
+    permitted = permitted.downcase
+    @people.push(Student.new('classroom', age, name))
+    puts "#{name.capitalize} was added as a student successfully"
+  end
+
   # Create a person method (teacher or student not a plain person)
   def create_a_person()
     puts 'Do you want to create a student(1) or a teacher(2)? [Enter a number 1 or 2]: '
     cartegory = gets.chomp.to_i
     case cartegory
     when 1
-      print 'Enter the student name: '
-      name = gets.chomp
-      print 'Enter the student\'s age: '
-      age = gets.chomp
-      print 'Does the student have parental permission? (true/false): '
-      permitted = gets.chomp
-      @people.push(Student.new('classroom', age, name))
-      puts "#{name.capitalize} was added as a student successfully"
+      create_student
     when 2
       print 'Enter the teacher name: '
       name = gets.chomp
