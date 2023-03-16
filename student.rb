bda7d5c51c447c30da73527899753b243990bde8
Botlhale Setou <botlhale.setou@gmail.com>
2023-03-16 18:42:00 +0200

    fix errors as per review

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: true)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def classroom=(classroom)
